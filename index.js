e7ac6a7a8dc3dd8dc1a99b0016852ca6d2c77de6
Yulia Kotenko <yulia.k@milestep.io>
2023-05-15 15:35:10 +0300

    changed displaying

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=5e347079&t=t
 
-const readline = require("readline");
-const axios = require("axios");
+const readline = require('readline');
+const axios = require('axios');
 
 const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=5e347079&s=';
 
-const drawDecorativeLine = () => console.log('=-=-=-=-=-=-=-=-=-=-=-=-');
+const drawEmptyLine = () => console.log('\n');
 
 function generateUrlWithTitle(inputtedParams) {
     const url = API_URL + inputtedParams;
@@ -18,15 +18,14 @@ function showFilms(films) {
 
 function showFilm(film) {
     const { Title, Year, Poster } = film;
+    drawEmptyLine();
     console.log(Title, `(${Year})`);
     console.log(Poster);
-    drawDecorativeLine();
 }
 
 async function startSearch (answer) {
     const searchURL = generateUrlWithTitle(answer);
     const response = await axios.get(searchURL);
-    drawDecorativeLine();
     showFilms(response.data.Search);
 }
 
